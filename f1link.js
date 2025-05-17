c9cf045cee3d71cdfe798e873d58435e4987439b
Ultrawebs by Harit <168084453+Ultrawebs@users.noreply.github.com>
2025-05-17 20:56:16 +0530

    Update f1link.js

--- f1link.js
+++ f1link.js
@@ -46,7 +46,7 @@ const videoLinks = {
   "emiliaromagna": {
     "FP1": "https://dai.fancode.com/primary/118411_english_hls_10486ta-di_h264_backup/vod_517_master.m3u8",
     "FP2": "https://dai.fancode.com/primary/118412_english_hls_19011ta-di_h264_backup/vod_107_master.m3u8",
-    "FP3": "<FP3_Link>",
+    "FP3": "https://dai.fancode.com/primary/118413_english_hls_47187ta-di_h264_backup/vod_798_master.m3u8",
     "Qualifying": "<Qualifying_Link>",
     "Race": "<Race_Link>"
   },
