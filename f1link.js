44e502db4d0afc7e4fdbb1d8fad894613e63e3cd
Ultrawebs by Harit <168084453+Ultrawebs@users.noreply.github.com>
2025-06-28 23:53:04 +0530

    Update f1link.js

--- f1link.js
+++ f1link.js
@@ -75,7 +75,7 @@ const videoLinks = {
     "Practice 1": "https://dai.fancode.com/secondary/118431_english_hls_70820ta-di_h264_backup/vod_272_master.m3u8",
     "Practice 2": "https://dai.fancode.com/secondary/118432_english_hls_14216ta-di_h264_backup/vod_81_master.m3u8",
     "Practice 3": "https://dai.fancode.com/secondary/118433_english_hls_13110ta-di_h264_backup/vod_841_master.m3u8",
-    "Qualifying": "link",
+    "Qualifying": "https://dai.fancode.com/secondary/118434_english_hls_84566ta-di_h264_backup/vod_953_master.m3u8",
     "Race": "link"
   }
 };
