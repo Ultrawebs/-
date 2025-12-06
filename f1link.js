3ffc30a6787d8bb8493e6ddc2856d5475264044f
Ultrawebs by Harit <168084453+Ultrawebs@users.noreply.github.com>
2025-12-06 22:57:37 +0530

    Update qualifying link for Abu Dhabi practices

--- f1link.js
+++ f1link.js
@@ -166,7 +166,7 @@ const videoLinks = {
     "Practice 1": "https://in-mc-vod.fancode.com/delhi/118496_english_hls_4add3c4fc669938_1ta-di_h264_backup/vod_703_index.m3u8",
     "Practice 2": "https://in-mc-vod.fancode.com/delhi/118497_english_hls_2fb6515cfe6260_1ta-di_h264_backup/vod_714_index.m3u8",
     "Practice 3": "https://in-mc-vod.fancode.com/delhi/118498_english_hls_8d6cdebf6830328_1ta-di_h264_backup/vod_253_index.m3u8",
-    "Qualifying": "link",
+    "Qualifying": "https://in-mc-vod.fancode.com/delhi/118499_english_hls_62b3635bd079005_1ta-di_h264_backup/vod_597_index.m3u8",
     "Race": "link"
   }
 };
